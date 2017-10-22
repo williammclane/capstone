f345f107139a3361fc1962eb4b5f833987965754
William McLane <dukeoftinmen@gmail.com>
2017-10-22 18:10:56 +0000

    ball moves

--- pong.js
+++ pong.js
@@ -13,6 +13,10 @@ var context = canvas.getContext("2d");
 var player = new Player();
 var computer = new Computer();
 var ball = new Ball((w / 2), (h / 2));
+ 
+function getRandInteger(min, max) { 
+    return Math.floor(Math.random() * (max - min) ) + min; 
+} 
 
 var animate = window.requestAnimationFrame || 
     window.webkitRequestAnimationFrame || 
@@ -64,12 +68,46 @@ function Computer() {
 function Ball(x, y) {
     this.x = x;
     this.y = y;
+    this.x_speed = getRandInteger(-10, 10); 
+    this.y_speed = getRandInteger(-10, 10); 
     this.radius = 8;
     this.render = function() {
         context.beginPath();
         context.arc(this.x, this.y, this.radius, 2 * Math.PI, false);
         context.fillStyle = "#FFFFFF";
         context.fill();
+    };
+    this.move = function(paddle1, paddle2) {
+        this.x += this.x_speed;
+        this.y += this.y_speed;
+        var top_x = this.x - 8;
+        var top_y = this.y - 8;
+        var bottom_x = this.x + 8;
+        var bottom_y = this.y + 8;
+
+        if (top_y < 0) {
+            this.y = 8;
+            this.y_speed = -this.y_speed;
+        } else if (bottom_y > h) {
+            this.y = h - 8;
+            this.y_speed = -this.y_speed;
+        }
+
+        if (top_x > (w / 2)){
+            if (top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x &&
+                top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y) {
+                    this.x_speed = this.x_speed * -1;
+                    this.y_speed += (paddle1.speed / 4);
+                    this.x += this.x_speed;
+            }
+        } else {
+            if (top_x < (paddle2.x + paddle2.width) && bottom_x > paddle2.x &&
+                top_y < (paddle2.y + paddle2.height) && bottom_y > paddle2.y) {
+                    this.x_speed = this.x_speed * -1;
+                    this.y_speed += (paddle2.speed / 4);
+                    this.x += this.x_speed;
+            }
+        }
     };
 }
 
@@ -78,6 +116,7 @@ function render() {
     player.render();
     computer.render();
     ball.render();
+    ball.move(computer.paddle, player.paddle); 
 }
 
 var step = function() {
@@ -92,8 +131,8 @@ window.onload = function() {
 window.addEventListener("keydown", function(event) {
     var k = event.keyCode;
     if (k == 38) {
-        player.paddle.move(-15);
+        player.paddle.move(-35);
     } else if (k == 40) {
-        player.paddle.move(15);
+        player.paddle.move(35);
     }
 });
