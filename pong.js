c8bfa06c2d39e4ae6254608c0d1ecc4bfe8eb1c5
William McLane <dukeoftinmen@gmail.com>
2017-10-23 02:11:08 +0000

    score works

--- pong.js
+++ pong.js
@@ -1,27 +1,63 @@
-var w = window.innerWidth ||
-        document.documentElement.clientWidth ||
-        document.body.clientWidth;
-
-var h = window.innerHeight ||
-        document.documentElement.clientHeight ||
-        document.body.clientHeight;
-
+var w = getWindowWidth(); 
+var h = getWindowHeight(); 
 var canvas = document.getElementById("blocPong");
+var context = canvas.getContext("2d"); 
 canvas.width = w;
 canvas.height = h;
-var context = canvas.getContext("2d");
 var player = new Player();
 var computer = new Computer();
 var ball = new Ball((w / 2), (h / 2));
  
-function getRandInteger(min, max) { 
-    return Math.floor(Math.random() * (max - min) ) + min; 
-} 
+var playerOneScore = 0; 
+var playerTwoScore = 0; 
 
 var animate = window.requestAnimationFrame || 
     window.webkitRequestAnimationFrame || 
     window.mozRequestAnimationFrame || 
     function(callback) { window.setTimeout(callback, 1000/60) }; 
+    
+var render = function() {
+    pongTable();
+    player.render();
+    computer.render();
+    ball.render();
+    ball.move(computer.paddle, player.paddle);
+    updateScore();
+};
+
+var update = function() {
+    computer.update(ball);
+};
+
+var step = function() {
+    update();
+    render();
+    animate(step);
+};
+
+function getRandInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+function getWindowHeight(){
+    return window.innerHeight ||
+        document.documentElement.clientHeight ||
+        document.body.clientHeight;
+}
+
+function getWindowWidth() {
+    return window.innerWidth ||
+        document.documentElement.clientWidth ||
+        document.body.clientWidth;
+}
+
+function updateScore() {
+    context.fillStyle = "white";
+    context.font = "20px 'Press Start 2P'";
+    context.textAlign = "center";
+    context.fillText("Score - " + playerOneScore, 150, 70);
+    context.fillText("Score - " + playerTwoScore, w - 150, 70);
+}
     
 function pongTable() {
     context.fillStyle = "#000000";
@@ -66,9 +102,9 @@ function Computer() {
     this.update = function(ball) { 
         var ballPos = ball.y; 
         
-        if (ballPos < (this.paddle.y + (this.paddle.height / 2) + 20)) { 
+        if (ballPos < (this.paddle.y)) { 
             this.paddle.move(-10); 
-        } else if (ballPos > (this.paddle.y + (this.paddle.height / 2) + 20)) { 
+        } else if (ballPos > (this.paddle.y + 80)) {      
             this.paddle.move(10); 
         } 
     }; 
@@ -101,12 +137,9 @@ function Ball(x, y) {
             this.y = h - 8;
             this.y_speed = -this.y_speed;
         }
-        if (this.x < 0 || this.x > w) { 
-            this.x_speed = -5; 
-            this.y_speed = 0; 
-            this.x = (w / 2); 
-            this.y = (h / 2); 
-        } 
+        
+        scoreGoal(this); 
+
         if (this.left > (w / 2)){ 
             if (this.left < (paddle1.x + paddle1.width) && this.right > paddle1.x && 
                 this.top < (paddle1.y + paddle1.height) && this.bottom > paddle1.y) { 
@@ -125,23 +158,32 @@ function Ball(x, y) {
     };
 }
 
-var render = function() {
-    pongTable();
-    player.render();
-    computer.render();
-    ball.render();
-    ball.move(computer.paddle, player.paddle); 
-}; 
+function scoreGoal(ball) { 
+    if (ball.x < 0) { 
+        playerTwoScore += 1; 
+        ball.x_speed = 5; 
+        ball.y_speed = 0; 
+        ball.x = (w / 2); 
+        ball.y = (h / 2); 
+    } 
+    if (ball.x > w) { 
+        playerOneScore += 1; 
+        ball.x_speed = -5; 
+        ball.y_speed = 0; 
+        ball.x = (w / 2); 
+        ball.y = (h / 2); 
+    } 
+} 
 
-var update = function() { 
-    computer.update(ball); 
-}; 
-
-var step = function() {
-    update(); 
+function resizeCanvas() { 
+    w = getWindowWidth(); 
+    h = getWindowHeight(); 
+    canvas.width = w; 
+    canvas.height = h; 
+    computer = new Computer(); 
+    player = new Player();  
     render();
-    animate(step);
-};
+}
 
 window.onload = function() {
     animate(step);
@@ -154,4 +196,6 @@ window.addEventListener("keydown", function(event) {
     } else if (k == 40) {
         player.paddle.move(35);
     }
-});
+}); 
+
+window.addEventListener("resize", resizeCanvas);
